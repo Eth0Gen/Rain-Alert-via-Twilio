34faecfac071ddbc6455ecc33a7b5e5ca5b7ef99
Eth0Gen <ethicszero015@gmail.com>
2026-03-30 20:15:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,34 +5,47 @@
 # 4. Update birthdays.csv to contain today's month and day.
 # See the solution video in the 100 Days of Python Course for explainations.
 
-
-from datetime import datetime
-import pandas
-import random
-import smtplib
 import os
+import requests
+from twilio.rest import Client
 
-# import os and use it to get the Github repository secrets
-MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
 
-today = datetime.now()
-today_tuple = (today.month, today.day)
+OWM_API_ENDPOINT = "https://api.openweathermap.org/data/2.5/forecast"
+MY_LAT = 22.944101
+MY_LONG = 88.433502
+API_KEY = os.environ.get("OWM_API_KEY")
+ACCOUNT_SID = os.environ.get(""OWM_ACCOUNT_SID")
+AUTH_TOKEN = os.enviorn.get("AUTH_TOKEN")
+TWILIO_PHONE_NUMBER = os.environ.get("TWILIO_PHONE_NUMBER")
+SEND_TO_PHONE_NUMBER = os.environ.get("TO_PHONE_NUMBER")
 
-data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
+parameter = {
+    "lat": MY_LAT,
+    "lon": MY_LONG,
+    "appid": API_KEY,
+    "cnt": 4
+}
 
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
-        connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
-        connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
-        )
+response = requests.get(url=OWM_API_ENDPOINT, params=parameter)
+response.raise_for_status()
+# stat_code = response.status_code
+weather_data = response.json()
+
+weather_forecast_list = weather_data["list"]
+
+will_rain = False
+
+for hour_data in weather_forecast_list:
+    condition_code = hour_data["weather"][0]["id"]
+    if int(condition_code) < 700:
+        will_rain = True
+
+if will_rain:
+    client = Client(ACCOUNT_SID, AUTH_TOKEN)
+    message = client.messages.create(
+        to=SEND_TO_PHONE_NUMBER,
+        from_=TWILIO_PHONE_NUMBER,
+        body="It's going to rain today. Remember to bring an Umbrella ☔!"
+    )
+    print(message.status)
+
